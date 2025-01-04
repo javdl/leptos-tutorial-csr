ef70f5af86c2d99872196370400ba2dbced016f3
J <2742259+javdl@users.noreply.github.com>
2025-01-04 14:38:05 +0100

    Add all deps to nix

--- flake.nix
+++ flake.nix
@@ -17,10 +17,18 @@
           devShell = with pkgs; mkShell {
             buildInputs = [ 
               cargo
+              rustc
+              wasm-pack
+              llvmPackages.bintools
               pkg-config 
               openssl 
-              trunk 
-            ];
+              trunk
+              (rust-bin.selectLatestNightlyWith( toolchain: toolchain.default.override {
+              extensions= [ "rust-src" "rust-analyzer" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })) ] ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
             CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
           };
